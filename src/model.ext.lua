801707a92ff899914f289346a375e6fde1b2715f
cryi <cryi@tutanota.com>
2021-06-19 00:49:11 +0200

    migration to latest ami

---
+++ src/model.ext.lua
@@ -0,0 +1,3 @@
+local _binaryUrl =  "https://github.com/Crowndev/crown-core/releases/download/v0.14.0.2-Emerald/Crown-0.14.0.2-Linux64.zip"
+
+am.app.set_model({DAEMON_URL = _binaryUrl}, {merge = true, overwrite = true})
